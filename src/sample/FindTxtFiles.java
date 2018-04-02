b64b53075aed3224e991551533992ce3a01e73dc
Trushenkov Dmitry <diman28051999@gmail.com>
2018-04-02 10:53:02 +0300

    Извлечен метод для поиска текстовых файлов в отдельный класс FindTxtFiles.

---
+++ src/sample/FindTxtFiles.java
@@ -0,0 +1,41 @@
+package sample;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * Класс для поиска текстовых файлов и запуска потоков для поиска текста в найденных текстовых файлах.
+ *
+ * @author Трушенков Дмитрий 15ИТ18
+ */
+public class FindTxtFiles {
+
+    /**
+     * Метод для поиска текстовых файлов в файловой системе и
+     * запуска потока для поиска заданного текста в найденных файлах.
+     *
+     * @param pathDirectory    путь начальной директории
+     * @param messageForSearch текст для поиска
+     */
+    public static ArrayList findFiles(File pathDirectory, String messageForSearch) {
+        ArrayList<SearchFilesAndText> arrayList = new ArrayList<>();
+        try {
+            File[] files = pathDirectory.listFiles();
+            assert files != null;
+            for (File file : files) {
+                if (file.isFile() && file.getName().matches(".*\\.txt$")) {
+
+                    arrayList.add(new SearchFilesAndText(file, messageForSearch));
+                    arrayList.get(arrayList.size() - 1).start();
+
+                } else if (file.isDirectory()) {
+                    findFiles(file, messageForSearch);
+                }
+            }
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+        return arrayList;
+    }
+
+}
